65eb1ddad36426d53bbe2a0ea2c2c6d67d7f54ff
Nahid HK <nahidappltd@gmail.com>
2024-07-30 22:38:31 +0600

    new update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 console.log("open Badminton App Js");
 
-function okpass(){
+function dataset(){
+    var logpopup = localStorage.getItem("setbox");
+    var loginio = localStorage.getItem("loginbox")
+    document.getElementById("popup1").style.display=logpopup; 
+    document.getElementById("popup2").style.display=loginio;   
+}
+
+function conpass(){
     var okpass = document.getElementById("okpass").value;
     var conpass = document.getElementById("conpass").value;
-    console.log(okpass);
-    console.log(conpass);
+if (okpass == conpass) {
+    document.getElementById("error").style.display="none"; 
+    document.getElementById("logbtn").style.display="block";
+} else {
+    document.getElementById("error").style.display="block";
+    document.getElementById("logbtn").style.display="none"; 
+}
+
+}
+function setpass(){
+    var conpass = document.getElementById("conpass").value; 
+    localStorage.setItem("password",conpass);
+    localStorage.setItem("setbox","none");
+    localStorage.setItem("loginbox","block");
+    document.getElementById("popup1").style.display="none";
+   window.location.href="/"
+}
+function login(){
+    var legel = localStorage.getItem("password");
+    var logdata = document.getElementById("login").value;
+    if (legel == logdata) {
+    document.getElementById("popup2").style.display="none";
+
+    } else {
+        
+    }
 }
