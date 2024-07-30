5538083068ed99a2e497b37c4ec9266a2d8a9f43
Nahid HK <nahidappltd@gmail.com>
2024-07-30 11:19:16 +0600

    new update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("open Badminton App Js");
+
+function okpass(){
+    var okpass = document.getElementById("okpass").value;
+    var conpass = document.getElementById("conpass").value;
+    console.log(okpass);
+    console.log(conpass);
+}
